056c9790871da841897988865a91337b1bf4acb1
darsuabasi <abasibasiurua@gmail.com>
2020-05-24 04:55:11 -0400

    backend

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const expess = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const userRouter = require("./routes/users")
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.use("/api/users", userRouter);
+app.use((err, req, res, next) => {
+    console.log(err);
+    if(err.status) {
+        res.status(err.status).json
+    } else {
+        res.status(500).json(err)
+    }
+})
+
+app.listen(PORT, () => {
+    console.log("Listening on port ", PORT);
+})
