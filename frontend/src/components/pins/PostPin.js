2d62022edd156b691643c4dd664a049ba64c4513
darsuabasi <abasibasiurua@gmail.com>
2020-07-22 23:59:54 -0400

    cleaning up dead code on Pin Modal

--- frontend/src/components/pins/PostPin.js
+++ frontend/src/components/pins/PostPin.js
@@ -1,42 +1,34 @@
 import React, { useState } from "react";
-// import Likes from '../Likes';
 import '../../css/PostPin.css';
-import { apiURL } from '../../util/apiURL';
+// import Likes from '../Likes';
+
 
 const PostPinModal = ({ imageurl, userName, pinContent, pinId }) => {
 
   const [modal, setModal] = useState(false);
-  const API = apiURL(); 
 
   const toggleModal = () => {
     setModal(!modal)
   }
 
-  const handleStylePin = {
-    height: "500px",
-    width: "600px",
-    border: "6px solid #E60023",
-  };
-
   
-
   return (
     <div className="mainDivModal "> 
         <div title ={pinId} className={`mainDivModalBackground showModal-${modal}`}>    
           <div className="innerModal">
               <div className="pinImage">
-                <img className="styleImage modal-content" alt="" src={imageurl} />
+                <img className="styleImage modal-content" alt="Board img" src={imageurl} />
               </div>
               <div className="pinContent">
                 <button className="closeButton card-img-top" onClick={() => toggleModal()}> X </button>
+                <h3 className="styleusername">{userName}</h3>
                 <p className="styleContent">{pinContent}</p>
-                <h4 className="styleusername">{userName}</h4>
               </div>
           </div>
         
         </div>
       <div className="overflow">
-        <img className="exitButton card-img-top" onClick={() => toggleModal()} src={imageurl} />
+        <img className="exitButton card-img-top" alt="Board img" onClick={() => toggleModal()} src={imageurl} />
       </div>
       
     </div>
