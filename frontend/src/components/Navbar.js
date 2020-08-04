21ecc53aed9d3785199d8107212f714e26d8f627
darsuabasi <abasibasiurua@gmail.com>
2020-08-03 20:16:29 -0400

    navbar w. userPic now showing and commented out empty components... will come back and clean code later

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,29 +1,61 @@
-import React, { useContext } from 'react';
-// import axios from 'axios';
-import { NavLink } from 'react-router-dom';
+import React, { useContext, useState, useEffect,  } from 'react';
+import axios from 'axios';
+import { NavLink, useHistory } from 'react-router-dom';
 // import Accounts from '../components/Accounts';
-import SearchBar from './SearchBar'
+import SearchBar from './SearchBar';
+import { apiURL } from '../util/apiURL';
+import { useInput } from '../util/useInput';
 import { AuthContext } from '../providers/AuthProvider';
 import '../css/Navbar.css'
 
 import { logout} from '../util/firebaseFunctions'
 
 
-const Navbar = (params) => {
+const Navbar = () => {
+    const { currentUser, token } = useContext(AuthContext);
+    const API = apiURL();
+    const [userId, setUserId] = useState("");
+    const history = useHistory("");
+    const [user, setUser] = useState([]);
+    const [profilepicture, setProfilePicture] = useState("");
 
-    const { currentUser } = useContext(AuthContext);
+    useEffect(() => {
+        const fetchUser = async () => {
+            // debugger
+            try {
+            let res = await axios({
+                method: "get",
+                url: `${API}/api/users/${currentUser.uid}`,
+                headers: {
+                    AuthToken: token,
+                },
+            });
+            debugger
+            setUserId(res.data.getUser.id);
+            setUser(res.data.getUser);
+            setProfilePicture(res.data.getUser.profilepic);
+            } catch (err) {
+                console.log(err.message)
+            }
+        };
+        fetchUser();
+    }, []);
+
+    // const { currentUser } = useContext(AuthContext);
     const displayNavForUser = () => {
         if(currentUser) {
             return( 
                 <div className="user-navBar">
+                
                     <NavLink className="pintrestLogoLetters" to={"/user-feed"}> P </NavLink>
                     <NavLink className="user-nav-feed" to={"/user-feed"}>Home</NavLink>
                     <NavLink className="user-nav-today" to={"/today"}>Today</NavLink>
-                    <NavLink className="user-nav-following" to={"/following"}>Following</NavLink>
+                    {/* <NavLink className="user-nav-following" to={"/following"}>Following</NavLink> */}
                     <SearchBar className="user-nav-search"> </SearchBar>
-                    <NavLink className="user-nav-notify" to={"/notifications"}> Notifications </NavLink>
+                    {/* <NavLink className="user-nav-notify" to={"/notifications"}> Notifications </NavLink> */}
                     <NavLink className="user-nav-message" to={"/messages"}> Messages </NavLink>
-                    <NavLink className="user-nav" to={"/user-profile/boards"}> ProfilePic </NavLink>
+                    <NavLink className="user-nav-image" to={"/user-profile/boards"}> <img className="user-nav-image-2" src={API+profilepicture}/> </NavLink>
+                    
                         <div className="dropdown"> 
                             <button className="user-nav-accounts" to={"/accounts"}>Accounts</button>
                                 <div className="account-dropdown-content">
@@ -38,13 +70,11 @@ const Navbar = (params) => {
                                     <a href="www.nothing.com"> Install the chrome app </a>
                                     <a href="www.nothing.com"> Get help </a>
                                     <a href="www.nothing.com"> See terms and privacy </a>
-                                    <button onClick={logout}>Logout</button>
-                            
-
+                                    <button className="logout-btn-style" onClick={logout}>Logout</button>
+                                </div>
+                        </div>
+ 
                     </div>
-                    </div>
-                    {/* <button onClick={logout}>Logout</button> */}
-                 </div>
              )  
         } else {        
             return (
@@ -64,112 +94,6 @@ const Navbar = (params) => {
                 </div>
             )
         }
-        // return(
-        //     <nav>
-        //         <div> 
-        //             <i class="icon ion-md-menu"> </i>
-        //             <i class="icon ion-md-close"> </i>
-        //         </div>
-
-        //         <a href="www.refreshuser.com" class="logo">  
-        //             <i class="pintrest-logo"> </i>
-        //         </a>
-
-        //         <ul className="full-nav-list">
-
-        //             <li>
-        //                 <NavLink to={"/user-home"}>Home</NavLink>
-        //             </li>
-
-        //             <li>
-        //             <NavLink className="publicNavLink" to={"https://business.pinterest.com/"}> Business</NavLink>
-        //             </li>
-
-        //             <li>
-        //                 <NavLink className="publicNavLink" to={"https://newsroom.pinterest.com/en"}> Blog </NavLink>
-        //             </li>
-
-        //             <li>
-        //                 {/* import login */}
-        //             </li>
-
-        //             <li>
-        //                 {/* import signup */}
-        //             </li>
-
-        //             <li>
-        //                 <NavLink to={"/user-home"}>Home</NavLink>
-        //             </li>
-
-        //             <li>
-        //                 <NavLink to={"/today"}>Today</NavLink>  
-        //             </li>
-
-        //             <li>
-        //                 <NavLink to={"/following"}>Following</NavLink>
-        //             </li>
-
-        //             <li>
-        //                 {/* <SearchBar/> */}
-        //             </li>
-
-        //             <li>
-        //                 <NavLink to={"/notifications"}> Notifications </NavLink>
-        //             </li>
-
-        //             <li>
-        //                 <NavLink to={'/messages'}> Messages </NavLink>
-        //             </li>
-
-        //             <li>
-        //                 <NavLink to={"/user/boards"}> ProfilePic </NavLink>
-        //             </li>
-
-        //             <li>
-        //                 <button> DD-Arrow</button>
-        //                 <ul className="menu-of-dropDown">
-        //                     <label> Accounts </label>
-        //                     <li>
-        //                         <button> Add another account </button>
-        //                     </li>
-
-        //                     <li>
-        //                         <button> Add a free business account </button>
-        //                     </li>
-
-        //                     <label> More Options</label>
-
-        //                     <li>
-        //                         <NavLink to={"/settings"}> Settings </NavLink>
-        //                     </li>
-
-        //                     <li>
-        //                         <NavLink to={"/edit"}> Tune your home feed </NavLink>
-        //                     </li>
-
-        //                     <li>
-        //                         <button> Install the chrome app </button>
-        //                     </li>
-
-        //                     <li>
-        //                         <a href="https://www.askforhelp.com"> Get help </a>
-        //                     </li>
-
-        //                     <li>
-        //                         <a href="https://www.seeaboutprivacy.com"> See terms and privacy </a>
-        //                     </li>
-
-        //                     <li>
-        //                         <button onClick={logout}>Logout</button>
-        //                     </li>
-        //                 </ul>
-        //             </li>
-        //         </ul>    
-        //     </nav>
-
-        // )
-
-            
     }
     return (
         <div> 
