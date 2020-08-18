06e9f1467729465ee9febcd1d57ae6d57dd823c9
darsuabasi <abasibasiurua@gmail.com>
2020-08-17 23:31:47 -0400

    restarted database for heroku deployment

--- backend/db/pintrest_db1.sql
+++ backend/db/pintrest_db1.sql
@@ -3,11 +3,11 @@
 
 -- \c pintrest_db;
 
-DROP TABLE IF EXISTS Users;
-DROP TABLE IF EXISTS Boards;
-DROP TABLE IF EXISTS Pins;
-DROP TABLE IF EXISTS Tags;
-DROP TABLE IF EXISTS Likes;
+-- DROP TABLE IF EXISTS Users;
+-- DROP TABLE IF EXISTS Boards;
+-- DROP TABLE IF EXISTS Pins;
+-- DROP TABLE IF EXISTS Tags;
+-- DROP TABLE IF EXISTS Likes;
 
 
 CREATE TABLE Users (
@@ -17,7 +17,6 @@ CREATE TABLE Users (
     last_name VARCHAR,
     bio TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    -- counts TEXT,
     profilePic VARCHAR,	
     email VARCHAR UNIQUE
 );
@@ -25,29 +24,18 @@ CREATE TABLE Users (
 CREATE TABLE Boards (
     id SERIAL PRIMARY KEY,
     board_name TEXT,
-    -- board_url TEXT,
     board_description TEXT,
     creator_id VARCHAR REFERENCES Users(id) ON DELETE CASCADE,
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    -- counts_data VARCHAR,
     board_image VARCHAR
-    -- pin_amount INT
 );
 
 CREATE TABLE Pins (
     id SERIAL PRIMARY KEY,
-    -- link VARCHAR UNIQUE,
     imageUrl VARCHAR,
     creator_id VARCHAR REFERENCES Users(id) ON DELETE CASCADE,
     board_id INT REFERENCES Boards(id) ON DELETE CASCADE,
-    -- created_at VARCHAR, 
     note NCHAR(200),
-    -- color TEXT, 
-    -- counts TEXT,
-    -- media TEXT,
-    -- attribution,
-    -- pin_image VARBINARY(max),
-    -- metadata, 
     time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
