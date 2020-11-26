c6e8a259b48848cb23231f26e5399b92d950e708
darsuabasi <abasibasiurua@gmail.com>
2020-11-25 20:38:10 -0500

    complete responsive skeleton for user nav + basic styling

--- frontend/src/components/navBar/Navbar.js
+++ frontend/src/components/navBar/Navbar.js
@@ -46,7 +46,69 @@ const Navbar = () => {
         if(currentUser) {
             return (
                 <div className="user-navBar">
-                    <h1> Hello owrld</h1>
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+                    {/* <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button> */}
+
+                        <NavLink className="pintrestLogoLetters" to={"/user-feed"}> P </NavLink>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
+
+                                <li class="nav-item active">
+                                    <NavLink class="nav-link" className="user-nav-feed" to={"/user-feed"}>Home <span class="sr-only">(current)</span> </NavLink>
+                                </li>
+                                        
+                                <li class="nav-item">
+                                    <NavLink class="nav-link" className="user-nav-today" to={"/today"}>Today</NavLink>
+                                </li>
+
+                                <SearchBar className="user-nav-search"> </SearchBar>
+
+                                {/* <form class="form-inline my-2 my-lg-0">
+                                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                                </form> */}
+
+
+                                <li class="nav-item">
+                                    <NavLink class="nav-link messages" className="user-nav-message" to={"/messages"}> Messages </NavLink>
+                                </li>
+
+                                <li class="nav-item">
+                                    <NavLink className="user-nav-image" to={"/user-profile/boards"}> <img className="user-nav-image-2" src={API+profilepicture}/> </NavLink> 
+                                </li>
+
+
+                                <div className="dropdown"> 
+                                    <button className="user-nav-accounts" to={"/accounts"}>Accounts</button>
+                                    
+                                    <div className="account-dropdown-content">
+                                        <label className="drop-labels">ACCOUNTS</label>
+                                            <a href="www.nothing.com"> Add another account </a>
+                                            <a href="www.nothing.com"> Add a free business account </a>
+                                        <label className="drop-labels"> MORE OPTIONS</label>
+                                   
+                                            <NavLink to={"/settings"}> Settings </NavLink>
+                                            <a href="www.nothing.com"> Tune your home feed </a>
+                                         
+                                            <a href="www.nothing.com"> Install the chrome app </a>
+                                            <a href="www.nothing.com"> Get help </a>
+                                            <a href="www.nothing.com"> See terms and privacy </a>
+                                            <button className="logout-btn-style" onClick={logout}>Logout</button>
+                                    </div>
+                                </div>
+
+                            </ul>
+                        </div>
+                    </nav>
+
 
                 </div>
             )
@@ -108,58 +170,10 @@ const Navbar = () => {
     return (
         <div> 
             <nav className="navbarUno"> 
-            {/* <NavLink to={"/"}> Login in</NavLink>   */}
                 {displayNavForUser()}
             </nav>
         </div>
     )
-
-
-
-
-
-
-
-//     return (
-//         <div>
-//             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-//                 <a class="navbar-brand" href="#">Navbar</a>
-//                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//                     <span class="navbar-toggler-icon"></span>
-//                 </button>
-
-//                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-//                     <ul class="navbar-nav mr-auto">
-//                     <li class="nav-item active">
-//                         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-//                     </li>
-//                     <li class="nav-item">
-//                         <a class="nav-link" href="#">Link</a>
-//                     </li>
-//                     <li class="nav-item dropdown">
-//                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-//                         Dropdown
-//                         </a>
-//                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-//                         <a class="dropdown-item" href="#">Action</a>
-//                         <a class="dropdown-item" href="#">Another action</a>
-//                         <div class="dropdown-divider"></div>
-//                         <a class="dropdown-item" href="#">Something else here</a>
-//                         </div>
-//                     </li>
-//                     <li class="nav-item">
-//                         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-//                     </li>
-//                     </ul>
-//                     <form class="form-inline my-2 my-lg-0">
-//                     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-//                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-//                     </form>
-//                 </div>
-// </nav>
-
-//     </div>
-//     )
 }
 
 export default Navbar
