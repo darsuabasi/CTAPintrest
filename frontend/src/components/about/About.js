423efee15e1b4ec24a1bad372ed47b545797c78a
darsuabasi <abasibasiurua@gmail.com>
2020-12-04 01:41:54 -0500

    re-do basic split skeleyon for about component

--- frontend/src/components/about/About.js
+++ frontend/src/components/about/About.js
@@ -7,6 +7,7 @@ const AboutMe = () => {
     <div className="aboutDiv">
 
         <div className="sub-div">
+          <div className="rightDiv"> 
             <p className="holaDiv"> Welcome to Lifetrest! </p>
 
             <p className="my-name-is"> Hi, my name is Uduakabasi but you can also call me Darsu. It's a play on my middle and last name. </p> 
@@ -15,6 +16,12 @@ const AboutMe = () => {
 
             <p> Check me out on <a className="instagram" href="https://www.instagram.com/darsu.chats/">Instagram</a> or <a className="twitter" href="https://twitter.com/darsuCodes">Twitter</a> and if you want to check out what I've been up to in regards to coding... here's my <a className="github" href="https://github.com/darsuabasi">Github</a>.
             </p> 
+          </div>
+
+          <div className="leftDiv">
+            <img/>
+
+          </div>
 
         </div>
 
