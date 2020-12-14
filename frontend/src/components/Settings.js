41768c2944c048112da208c35853b7e6110fd1a1
darsuabasi <abasibasiurua@gmail.com>
2020-12-14 12:29:05 -0500

    disable links in settings- default display of edit profile which is disabled for now

--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import EditProfile from './EditProfile'
+import EditProfile from './editProfile/EditProfile'
 import '../css/Settings.css'
 
 const Settings = () => {
     return(
-        <div>
-
+        <div className="fullSettingsDiv">
             <div className="listed-options-display">
                 <ul className="list-style-display">
-                    <li class="setting-icon"><a href="/settings/edit-profile" className="editing-style">Edit Profile</a></li>
+                    <li class="setting-icon disabled"><a href="/settings/edit-profile" className="editing-style">Edit Profile</a></li>
                     <li class="account-icon disabled"><a href="/settings/account-settings" className="account-set-style">Account Settings</a></li>
                     <li class="claim-icon disabled"><a href="/settings/claim" className="claim-style">Claim</a></li>
                     <li class="notif-icon disabled"><a href="/settings/notifications" className="notif-style">Notifications</a></li>
