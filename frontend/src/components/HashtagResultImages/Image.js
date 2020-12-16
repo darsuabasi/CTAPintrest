853f8b98436ce0fb64278916d8585be8046a4717
darsuabasi <abasibasiurua@gmail.com>
2020-12-15 23:48:28 -0500

    complete skeleton and css styling for pin results from hashtag search

---
+++ frontend/src/components/HashtagResultImages/Image.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './images.css'
+
+const PostImage = ({ imageurl, userName, profilepic, pinContent, pinId }) => {
+  const handleStyleProfile = {
+    height: "80px",
+    width: "80px"
+  };
+  const handleStylePost = {
+    height: "auto",
+    width: "420px"
+  };
+
+  return (
+    <div class="imageCard" title ={pinId}>
+      <img className="styleImgForPin" class="image__img" src={imageurl} alt="" style={handleStylePost}/>
+      <div class="image__overlay image__overlay--primary">
+        <img className="styleProfilePicOverlay" alt="" src={profilepic} style={handleStyleProfile} />
+          <div class="image__title">{userName}</div>
+          <p class="image__description"> Info: {pinContent} </p>
+      </div>
+    </div>
+  );
+};
+
+export default PostImage;
