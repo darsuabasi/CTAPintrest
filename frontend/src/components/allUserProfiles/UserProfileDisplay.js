aefa9c63022b8200d71990cdb2757357edde21eb
darsuabasi <abasibasiurua@gmail.com>
2020-12-17 00:09:01 -0500

    displaying user information + their pins on all user profiles

--- frontend/src/components/allUserProfiles/UserProfileDisplay.js
+++ frontend/src/components/allUserProfiles/UserProfileDisplay.js
@@ -1,48 +1,70 @@
-import React from 'react';
-import Container from 'react-bootstrap/esm/Container';
+import React, {useState, useEffect, useContext} from 'react';
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import './css/userprofiledisplay.css';
+import { AuthContext } from "../../providers/AuthProvider";
+import { apiURL } from "../../util/apiURL";
+import UserInfo from './UserInfo'
+import UserPins from "./UserPins";
+// import UserBoards from "./UserBoards";
+
 
 const UserProfile = () => {
+    const API = apiURL();
+    const { username } = useParams();
+    const [users, setUsers] = useState([]);
+    const { token, loading } = useContext(AuthContext);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                let res = await axios({
+                    method: "get",
+                    url: `${API}/api/users`
+                })
+                setUsers(res.data.payload)
+            } catch(err) {
+                console.log(err.message);
+                }
+            };
+            fetchUser()
+    }, [])
+
+    const selectedUser = users.map((user, i) => {
+        if (username === user.username) 
+        return (
+            <div className="userProfile-main-container">
+                <div className="userProfile-top-div" title={i}>
+                    <UserInfo key={i} id={user.id} username={user.username} first_name={user.first_name} last_name={user.last_name} bio={user.bio} created_at={user.created_at} profilePic={API+user.profilepic} email={user.email}/>
+                </div>
+
+                {/* <div className="userprofile-user-fullname-footer-div">
+                    <h1 className="userprofile-user-fullname"> FirstName + LastName</h1>
+                </div> */}
+
+                {/* <div className="userProfile-bottom-div">
+                    <UserPins/>
+                </div> */}
+
+            </div>
+        )
+    });
+    console.log(selectedUser);
+
+    if (loading) return <div>Loading...</div>;
+
     return (
         <div className="userProfile-main-container">
+            {/* <div className="userProfile-top-div"> */}
+                {selectedUser}    
+            {/* </div> */}
 
-            <div className="userProfile-top-div">
-                <h1> Profile Picture</h1>
-                <h1> FirstName LastName</h1>
-                <h1> @Username</h1>
-                <h1> Followers + Following</h1>
-                <h1> Bio: </h1>
-                <h1> FillerSpace</h1>
-                <h1> FillerSpace</h1>
-            </div>
-
-            <div className="userprofile-user-fullname-footer-div">
-                <h1 className="userprofile-user-fullname"> FirstName + LastName</h1>
-            </div>
 
             <div className="userProfile-bottom-div">
-                <h1> Second div</h1>
-                <h1> Second div</h1>
-                <h1> Second div</h1>
-                <h1> Second div</h1>
-                <h1> Second div</h1>
-                <h1> Second div</h1>
-                <h1> Second div</h1>
-                <h1> Second div</h1>
-                <h1> Second div</h1>
-                <h1> Second div</h1>
-                <h1> Second div</h1>
-                <h1> Second div</h1>
-                <h1> Second div</h1>
-                <h1> Second div</h1>
-                <h1> Second div</h1>
-                <h1> Second div</h1>
-
+                <UserPins username={username}/>
             </div>
-
         </div>
-    )
-
+    );
 }
 
 
