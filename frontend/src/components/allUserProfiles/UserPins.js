446dcf3a894ff11e950ba2b088e143aff45cf2ef
darsuabasi <abasibasiurua@gmail.com>
2020-12-16 23:59:25 -0500

    create userPins to be exported to users profile component

---
+++ frontend/src/components/allUserProfiles/UserPins.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState, useContext } from 'react';
+import PostPinModal from '../pins/allPins/PostPin';
+import axios from "axios";
+import { AuthContext } from "../../providers/AuthProvider";
+import { apiURL } from "../../util/apiURL";
+
+
+const UserPins = ({username}) => {
+    const API = apiURL();
+    const { token, loading } = useContext(AuthContext);
+    const [userPins, setUserPins] = useState([]);
+  
+
+    useEffect(() => {
+        const fetchUserPins = async () => {
+            try {
+                let res = await axios({
+                    method: "get",
+                    url: `${API}/api/pins`,
+                    // headers: {
+                    //     AuthToken: token,
+                    // }
+                })
+                debugger
+                setUserPins(res.data.payload)
+                console.log(res.data)
+            } catch(err) {
+                setUserPins([])
+                console.log(err.message);
+                }
+            };
+            fetchUserPins();
+    }, [username]);
+
+      const userPinList = userPins.map((pin, i) => {
+        if (username === pin.username)
+          return (
+            <div key={i}>
+                <PostPinModal key={pin.id} pinId={pin.id} userName={pin.username} imageurl={API+pin.imageurl} profilepic={API+pin.profilepic} pinContent={pin.note} />
+            </div> 
+          );
+      });
+    
+      if (loading) return <div>Loading...</div>;
+    
+      return (
+        <>
+          <div className="user-pin-list-div">
+            <h1 className="user-pins-now-viewing">Pins</h1>
+            <div>{userPinList}</div>
+          </div>
+        </>
+      );
+    };
+
+export default UserPins;
