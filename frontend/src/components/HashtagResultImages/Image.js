541a4a4dede2915f58fc0b641eb1adc4eea8d32f
darsuabasi <abasibasiurua@gmail.com>
2020-12-17 16:47:03 -0500

    add redirect to user profile thru picture and username on pins from hashtag search results

--- frontend/src/components/HashtagResultImages/Image.js
+++ frontend/src/components/HashtagResultImages/Image.js
@@ -1,7 +1,13 @@
 import React from "react";
-import './images.css'
+import './images.css';
+import { useHistory } from "react-router-dom";
 
 const PostImage = ({ imageurl, userName, profilepic, pinContent, pinId }) => {
+
+  const history = useHistory();
+ 
+    const userNameRedirect = (userName) => history.push(`/users/${userName}/pins`);
+
   const handleStyleProfile = {
     height: "80px",
     width: "80px"
@@ -15,8 +21,8 @@ const PostImage = ({ imageurl, userName, profilepic, pinContent, pinId }) => {
     <div class="imageCard" title ={pinId}>
       <img className="styleImgForPin" class="image__img" src={imageurl} alt="" style={handleStylePost}/>
       <div class="image__overlay image__overlay--primary">
-        <img className="styleProfilePicOverlay" alt="" src={profilepic} style={handleStyleProfile} />
-          <div class="image__title">{userName}</div>
+        <img onClick={() => userNameRedirect(userName)}  className="styleProfilePicOverlay" alt="" src={profilepic} style={handleStyleProfile} />
+          <div onClick={() => userNameRedirect(userName)} class="image__title">{userName}</div>
           <p class="image__description"> INFO: {pinContent} </p>
       </div>
     </div>
