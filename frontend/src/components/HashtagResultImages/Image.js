679435756ff8a0a30322bcbfae4f32c4658754bb
darsuabasi <abasibasiurua@gmail.com>
2020-12-19 17:06:08 -0500

    still working on hashtag display on pin modal when opened... still developing

--- frontend/src/components/HashtagResultImages/Image.js
+++ frontend/src/components/HashtagResultImages/Image.js
@@ -1,21 +1,39 @@
-import React from "react";
-import './images.css';
+import React, { useState, useEffect }  from "react";
 import { useHistory } from "react-router-dom";
+// import axios from "axios";
+// import { AuthContext } from '../../providers/AuthProvider';
+// import { apiURL } from '../../util/apiURL';
+import './images.css';
 
-const PostImage = ({ imageurl, userName, profilepic, pinContent, pinId }) => {
+const PostImage = ({ imageurl, userName, profilepic, pinContent, pinId, tag_name }) => {
 
-  const history = useHistory();
- 
+    const history = useHistory();
     const userNameRedirect = (userName) => history.push(`/users/${userName}/pins`);
+    // const [tags, setTags] = useState([]);
+    // const API = apiURL();
 
-  const handleStyleProfile = {
-    height: "80px",
-    width: "80px"
-  };
-  const handleStylePost = {
-    height: "auto",
-    width: "420px"
-  };
+    // const fetchTags = async () => {
+    //   try {
+    //     let res = await axios.get(`${API}/api/tags/${sessionStorage.searchTerm}`);
+    //     setTags(res.data.payload);
+    //   } catch (error) {
+    //     setTags([]);
+    //   }
+    // };
+  
+  
+    // useEffect(() => {
+    //   fetchTags(`${API}/api/tags/${pinId}`);
+    // }, [] );
+
+    const handleStyleProfile = {
+      height: "80px",
+      width: "80px"
+    };
+    const handleStylePost = {
+      height: "auto",
+      width: "420px"
+    };
 
   return (
     <div class="imageCard" title ={pinId}>
@@ -23,7 +41,8 @@ const PostImage = ({ imageurl, userName, profilepic, pinContent, pinId }) => {
       <div class="image__overlay image__overlay--primary">
         <img onClick={() => userNameRedirect(userName)}  className="styleProfilePicOverlay" alt="" src={profilepic} style={handleStyleProfile} />
           <div onClick={() => userNameRedirect(userName)} class="image__title">{userName}</div>
-          <p class="image__description"> INFO: {pinContent} </p>
+          <p class="image__description"> CAPTION <p>{pinContent}</p></p>
+          {/* <p> {tag_name} </p> */}
       </div>
     </div>
   );
