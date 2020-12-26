0ed19f4e66cda2733c61a939941a27efed3efbf5
darsuabasi <abasibasiurua@gmail.com>
2020-12-26 18:36:11 -0500

    remove headers and netlify.toml file + also change server link

--- frontend/src/util/apiURL.js
+++ frontend/src/util/apiURL.js
@@ -1,6 +1,6 @@
 export const apiURL = () => {
     return window.location.hostname === "localhost" 
     ? "http://localhost:3005" 
-    : "https://lifetrest.herokuapp.com";
+    : "https://my-lifetrest-app.herokuapp.com";
 };
 
